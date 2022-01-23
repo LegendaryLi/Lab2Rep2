0efc710f0f7deb4fd8574cf7d2e15e0464dd06a0
LegendaryLi <cofgtweart2@gmail.com>
2022-01-23 11:17:23 +0000

    added func get_mult

--- GitProgLab2/Program.cs
+++ GitProgLab2/Program.cs
@@ -15,7 +15,7 @@ int var = Convert.ToInt32(Console.ReadLine());
     {
         /*case 1: c = a + b; Console.WriteLine("result:" + c); break;*/
         /*case 2: c = a - b; Console.WriteLine("result:" + c); break;*/
-        /*case 3: c = a * b; Console.WriteLine("result:" + c); break;*/
+        case 3: c = a * b; Console.WriteLine("result:" + c); break;
         /*case 4: c = a / b; Console.WriteLine("result:" + c); break;*/
         default: Console.WriteLine("Этот ввод не определен"); break;
     }
