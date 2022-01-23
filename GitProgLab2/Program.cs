d1b4d38bb6208ea2b9b2cb750a301986e4f73fab
LegendaryLi <cofgtweart2@gmail.com>
2022-01-23 11:06:42 +0000

    added menu

---
+++ GitProgLab2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Welcome to my little calculator 3000!");
+Console.WriteLine("Please entry A:");
+/*int a = Convert.ToInt32(Console.ReadLine());*/
+Console.WriteLine("Please entry B:");
+/*int b = Convert.ToInt32(Console.ReadLine());*/
+int c;
+Console.WriteLine("Please entry number for select:");
+Console.WriteLine("1 - <+>");
+Console.WriteLine("2 - <->");
+Console.WriteLine("3 - <*>");
+Console.WriteLine("4 - </>");
+int var = Convert.ToInt32(Console.ReadLine());
+    switch (var)
+    {
+        /*case 1: c = a + b; Console.WriteLine("result:" + c); break;*/
+        /*case 2: c = a - b; Console.WriteLine("result:" + c); break;*/
+        /*case 3: c = a * b; Console.WriteLine("result:" + c); break;*/
+        /*case 4: c = a / b; Console.WriteLine("result:" + c); break;*/
+        default: Console.WriteLine("Этот ввод не определен"); break;
+    }
